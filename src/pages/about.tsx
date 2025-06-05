b197b1676c86afd5ab69b424693538656ff75641
microtower00 <microtower00@gmail.com>
2025-06-06 00:16:11 +0200

    feat: :tada: Created project, initial folder structure

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+const AboutPage: React.FC<PageProps> = () => {
+  return (
+    <>
+      <p>So Michee</p>
+    </>
+  );
+};
+export default AboutPage;
