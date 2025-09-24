70ad9dccd3c14a23a84f295692517ea04dcbad0d
microtower00 <microtower00@gmail.com>
2025-09-24 14:08:49 +0200

    Added table to instagram

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Portfolio`,
-    siteUrl: `microtower00.github.io`,
+    siteUrl: `https://microtower00.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
