fa3fe13f565cdbf5de29b6099adfc1501b7a3d3a
microtower00 <microtower00@gmail.com>
2025-09-28 21:58:54 +0200

    Full working timeline!

--- src/components/TimelineWrapper.tsx
+++ src/components/TimelineWrapper.tsx
@@ -1,82 +1,59 @@
 import React from "react";
 import TimelineEntry from "./ui/TimelineEntry";
-import { LucideArrowRight, LucideSmile } from "lucide-react";
+import { LucideArrowRight, LucideDownload, LucideSmile } from "lucide-react";
 import timelineData from "../content/timelineData.json";
 
 interface TimelineEntryData {
-  icon: string; // This will be the image path
+  icon: string;
   company: string;
   startEnd: string;
+  href?: string;
 }
 
 const TimelineWrapper: React.FC = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        width: "100%",
-        maxWidth: "800px",
-        position: "relative",
-      }}
-    >
-      {timelineData.entries.map((entry: TimelineEntryData, index: number) => {
-        const iconNode = (
-          <img
-            src={entry.icon}
-            alt={`${entry.company} logo`}
-            style={{
-              width: "32px",
-              height: "32px",
-              objectFit: "cover",
-            }}
-          />
-        );
-        return (
-          <TimelineEntry
-            key={index}
-            latest={index === 0}
-            icon={iconNode}
-            company={entry.company}
-            startEnd={entry.startEnd}
-          />
-        );
-      })}
+    <div className="timeline-wrapper">
+      <div className="timeline-wrapper">
+        {timelineData.entries.map((entry: TimelineEntryData, index: number) => {
+          const iconNode = (
+            <img
+              src={entry.icon}
+              alt={`${entry.company} logo`}
+              className="timeline-icon-image"
+            />
+          );
+          return (
+            <TimelineEntry
+              key={index}
+              latest={index === 0}
+              icon={iconNode}
+              company={entry.company}
+              startEnd={entry.startEnd}
+              href={entry.href ? entry.href : undefined}
+            />
+          );
+        })}
+      </div>
 
-      {/* <div
-        style={{
-          position: "absolute",
-          right: "0px",
-          height: "100%",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "flex-start",
-        }}
-      >
+      <div className="timeline-see-all-container">
+        <div className="timeline-gradient-overlay"></div>
         <div
           style={{
-            width: "32px",
             height: "100%",
-            background:
-              "linear-gradient(to left, var(--primary-color), transparent)",
-          }}
-        ></div>
-        <div
-          id="seeAll"
-          className="label"
-          style={{
             backgroundColor: "var(--primary-color)",
-            height: "100%",
-            fontSize: "18px",
-            display: "flex",
-            flexDirection: "row",
-            padding: "4px 0",
-            alignItems: "flex-start",
           }}
         >
-          all <LucideArrowRight />
+          <a
+            href="/CV.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="label timeline-see-all-button"
+          >
+            <LucideDownload size="16px" />
+            CV
+          </a>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
--- src/components/ui/TimelineEntry.tsx
+++ src/components/ui/TimelineEntry.tsx
@@ -5,6 +5,7 @@ interface TimelineEntryProps {
   icon: React.ReactNode;
   company: string;
   startEnd: string;
+  href?: string;
 }
 
 const TimelineEntry: React.FC<TimelineEntryProps> = ({
@@ -12,95 +13,28 @@ const TimelineEntry: React.FC<TimelineEntryProps> = ({
   icon,
   company,
   startEnd,
+  href,
 }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "4px",
-        minWidth: "120px",
-        maxWidth: "200px",
-        width: "100%",
-      }}
-    >
-      <div
-        className={`${latest ? "latest" : ""}`}
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          position: "relative", // Added for positioning the before element
-          gap: "2px",
-        }}
-      >
-        {latest && (
-          <div
-            id="beforeElement"
-            style={{
-              content: '""', // Required for pseudo-element
-              position: "absolute",
-              width: "32px",
-              height: "32px",
-              backgroundColor: "rgba(0, 128, 0, 0.2)",
-              borderRadius: "999px",
-            }}
-          />
-        )}
-
+    <div className="timeline-entry">
+      <div className={`timeline-line-container ${latest ? "latest" : ""}`}>
+        {latest && <div className="timeline-before-element" />}
         <div
-          id="cvCircle"
-          style={{
-            borderRadius: "999px",
-            boxShadow: latest
-              ? "none"
-              : "inset 0 0 0 1px var(--gray-text-color)", // Added box shadow effect
-            width: "8px",
-            height: "8px",
-            aspectRatio: "1 / 1",
-            backgroundColor: latest ? "var(--green-accent)" : "",
-            margin: "12px 0 12px 12px ",
-          }}
+          className={`timeline-circle ${
+            latest ? "timeline-circle-latest" : ""
+          }`}
         />
         <div
-          style={{
-            top: "50%",
-            left: "0",
-            width: "100%",
-            height: "2px",
-            background: latest
-              ? "linear-gradient(to right, var(--green-accent), var(--surface-color)"
-              : "linear-gradient(to right, var(--surface-color), var(--gray-text-color), var(--surface-color)", // Horizontal gradient line
-          }}
+          className={`timeline-line ${latest ? "timeline-line-latest" : ""}`}
         />
       </div>
-      <div
-        style={{
-          padding: " 4px 0 4px 0",
-          display: "flex",
-          alignItems: "center",
-          gap: "8px",
-        }}
-      >
-        <div
-          style={{
-            border: "1px solid white",
-            borderRadius: "8px",
-            aspectRatio: "1 / 1", // Maintain a square aspect ratio
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "32px",
-            overflow: "hidden",
-          }}
-        >
-          {icon}
-        </div>
-        <span className="bodyS">{company}</span>
+      <div className="timeline-content">
+        <div className="timeline-icon-container">{icon}</div>
+        <a href={href} className="bodyM timeline-company">
+          {company}
+        </a>
       </div>
-      <div className="label" style={{ color: "var(--gray-text-color)" }}>
-        {startEnd}
-      </div>
+      <div className="timeline-date label">{startEnd}</div>
     </div>
   );
 };
